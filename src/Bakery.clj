82eb8577bd8ff9beac05ce53ceaa378984f53260
John McCall <mcca0798@zytel.morris.umn.edu>
2013-02-18 19:04:21 -0600

    Made a lot of progress. Ended up sorta not working.

--- src/Bakery.clj
+++ src/Bakery.clj
@@ -19,16 +19,8 @@
   )
 
 
-;; Make People
-(defn make-people [c s]
-  (let [customerList [] serverList []]
-    (for [i (range c)] (conj customerList (customer i)))
-    (for [i (range s)] (conj serverList (server (+ i c))))
-  )
-)
-
 ;; Free servers
-(def free-servers (ref {}))
+(def free-servers (ref #{}))
 
 ;; The Ticket Machine
 (def ticket-machine (atom -1))
@@ -37,32 +29,89 @@
   (swap! ticket-machine inc))
 
 ;; The "Now Serving" sign
-(def now-serving-sign (ref 0))
+(def now-serving-sign (atom 0))
 
+(defn serve-next [id reference oldValue newValue] 
+  (if (not (empty? @reference))
+	  (do (swap! now-serving-sign inc)
+        (println "Now serving:" @now-serving-sign))
+    nil
+    )
+  )
+
+;; Making Pastries
+(defn make-pastries [server]
+  (let [pastry-number (rand-int 50)]
+  (print "computing the fib of" pastry-number)
+  (send server #(assoc %1 :pastry %2) (fib pastry-number))
+  )
+)
+
+;; Serve the Customer
 (defn finished-serving [server]
   (dosync
-    (alter now-serving-sign inc)
-    (println "Now serving:" @now-serving-sign)
     (alter free-servers #(conj %) server)
     (println "Server number:" (@server :id) "is now free")
     (send server #(assoc %1 :pastry %2) nil)
     )
   )
 
+(defn start-serving [server customer]
+  (println "Server " (:id @server) " is helping Customer " (:id @customer) " by ")
+  (future (make-pastries server))
+  (let [pastry (:pastry @server)]
+    (send customer #(assoc %1 :result %2) pastry)
+    )
+  )
+
+(defn get-server []
+  (let [the-server (first @free-servers)]
+    (disj @free-servers the-server)
+    (println "Server " (:id @the-server) " is now busy")
+    the-server
+    )
+  )
+
+(defn serve-customer [customer]
+  (let [the-server (get-server)]
+	  (start-serving the-server customer)
+	  (finished-serving the-server)
+   )
+  )
+
 ;; Take a number
 ;; Give the customer a number and add the customer to the watch.
 (defn take-a-number [customer]
-  
+  (next-number)
+  (send customer #(assoc %1 :ticket-number %2) @ticket-machine)
+  (add-watch now-serving-sign customer watch-sign)
+  (println "Customer " (:id @customer) "took number: " @ticket-machine)
   )
 
-(defn watch []
-  
+(defn watch-sign [customer reference oldValue newValue]
+  (if (= newValue (@customer :ticker-number))
+    (serve-customer customer)
+    nil
+    )
   )
 
-;; Making Pastries
-(defn make-pastries [server]
-  (send server #(assoc %1 :pastry %2) (fib (rand-int 50)))
+;; Make People
+(defn make-people [c s]
+  (let [customerList (transient [])]
+    (for [i (range c)] (conj! customerList (customer i)))
+    (for [i (range s)] (alter free-servers #(conj %1 (server (+ i c)))))
+    ;;(alter free-servers (fn [] serverList))
+    (add-watch free-servers 0 serve-next)
+   ;; (println (empty? (persistent! customerList)))
+    (map (fn [customer] 
+           (do (Thread/sleep (rand-int 1000))
+               (take-a-number customer)
+           )
+         ) (persistent! customerList)
+     )
   )
+)
+
 
 
 ;; Tests!!
